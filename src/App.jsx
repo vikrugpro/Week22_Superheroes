61bab661888e7ba71b2a24b9822b7a4fac7ca747
vikrugpro <vikrugpro@gmail.com>
2025-06-19 13:59:51 +0300

    clear code

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,18 @@ function App() {
       {data.map((i) => (
         <Card
           key={i.id}
-          name={i.name}
-          universe={i.universe}
-          alterego={i.alterego}
-          occupation={i.occupation}
-          friends={i.friends}
-          superpowers={i.superpowers}
-          url={i.url}
-          info={i.info}
+          {...i}
+          // Выше короткий способ передачи props. по сути ниже тот же результат, но длинным способом, прописывая вручную все пропсы
+
+          // name={i.name}
+          // universe={i.universe}
+          // alterego={i.alterego}
+          // occupation={i.occupation}
+          // friends={i.friends}
+          // superpowers={i.superpowers}
+          // url={i.url}
+          // info={i.info}
+          {...i}
         />
       ))}
     </div>
